1f8dee583da525e31b25a2f1b50f8d7697162749
kyle <kroscoe45@gmail.com>
2025-01-18 17:53:41 -0800

    reworrking

--- pagescraper.py
+++ pagescraper.py
@@ -24,6 +24,24 @@ def scrape_page(url : str) -> object:
         Formatted JSON object containing scraped data
     TODO: explain JSON object format here
     '''
+    # region helper functions
+    def is_internal_link(href, base_url):
+        parsed_href = urlparse(href)
+        parsed_base = urlparse(base_url)
+        return parsed_href.netloc == '' or parsed_href.netloc == parsed_base.netloc
+    def link_reduce(acc, a):
+        # Ignore links to page HTML elements and <a> tags with no href
+        if not (href := a.get('href')) or href.startswith("#"):
+            return acc
+        link_entry  ={
+            
+            'title' : a.get('title'),
+            'text'  : a.get_text(),
+            'url'   : urljoin(url, href)
+        }
+        acc[['internal', 'external'][int(is_internal_link(href, url))]].append(href)
+        return acc
+    # endregion helper functions
     try:
         # Fetch the webpage
         response = requests.get(url)
@@ -31,57 +49,18 @@ def scrape_page(url : str) -> object:
         soup = BeautifulSoup(response.content, 'html.parser')
     except Exception as e:
         return {"error": f"Failed to scrape {url}: {e}"}
-    
-    domain = urlparse(url).netloc
     # extract all the links on the page
-    links_on_page = soup.find_all('a')
-    def link_reduce(acc, a):
-        href = a.get('href')
-        if not href:
-            return acc
-        if href.startswith("#"):
-            # Ignore internal page links
-            return acc
-        full_url = urljoin(url, href)  # Resolve relative URLs to absolute
-        parsed_url = urlparse(full_url)
-        if href.startswith("//"):
-            # Protocol-relative external link
-            '''
-             classification = 'external'
-            acc["external"].append(
-                {
-                    'title': a.get('title'),
-                    'text': a.get_text(),
-                    'url': full_url
-                
-                }'''
-            acc['external'].append(full_url)
-        elif href.startswith("/"):
-            # Absolute path within the domain
-            acc["internal"].append(full_url)
-        elif parsed_url.netloc and parsed_url.netloc != domain:
-            # Full URL pointing to an external domain
-            acc["external"].append(full_url)
-        else:
-            # Relative path or fragment (internal link)
-            acc["internal"].append(full_url)
-        return acc
-        
-    sorted_links = reduce(link_reduce, links_on_page, {'internal': [], 'external': []})
-    # now that we have all the links, we can remove them from the soup
-    # by replacing them with their text to remove hyperlinks
-    for link in links_on_page:
-        link.replaceWith(link.get_text())
-    # remove all style tags (not really useful for ai analysis)
-    for tag in soup.find_all(['b', 'i', 'u', 'strong', 'em', 'span']):
-        tag.unwrap()
-    for named_div in [x for x in soup.find_all('div') if x.get('title')]:
-        print(named_div.get('title'))
-        div_title = named_div.get('title')
-        named_div.replaceWith(soup.new_tag('div', title=div_title))
-        soup.prettify()
+    def serialize(element):
+        if not (tag := element.name):
+            return element
+        return {  
+            'tag'           : element.name,
+            'attributes'    : element.attrs,
+            'children'      : [el for el in element.children if el.name not in UNWANTED_ENCLOSING_TAGS],
+        }
+    serialized_soup = serialize(soup.body)
     with open("test.html", "w", encoding="utf-8") as file:
-        file.write(str(soup))
+        file.write(str(serialized_soup))
     print('break') 
 
 
