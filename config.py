2dc4f3624a013b8bbaf6d840c9b1c6b0a59ce537
kyle <kroscoe45@gmail.com>
2025-01-20 17:02:13 -0800

    ,,

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # region scraper settings 
 URL = 'https://en.wikipedia.org/wiki/Volcanism_of_the_Mount_Edziza_volcanic_complex'
-UNWANTED_TAGS = ['']
+UNWANTED_TAGS = ['button']
 UNWANTED_ENCLOSING_TAGS = ['header', 'footer', 'nav', 'aside', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6']
 # endregion
 
--- pagescraper.py
+++ pagescraper.py
@@ -7,9 +7,42 @@ or any non-managed interface
 import requests
 from bs4 import BeautifulSoup
 import json
-from config import URL, UNWANTED_TAGS
+from config import URL, UNWANTED_ENCLOSING_TAGS, UNWANTED_TAGS
 from urllib.parse import urlparse, urljoin
 from functools import reduce
+
+# region helper functions
+def is_internal_link(href, base_url):
+    if href.startswith('/') or href.startswith('#'):
+        return True
+    return False
+def link_reduce(acc, a):
+    # Ignore links to page HTML elements and <a> tags with no href
+    if not (href := a.get('href')) or href.startswith("#"):
+        return acc
+    name = a.get('title') or a.get('aria-label') or ''
+    link_entry  ={
+        'name'  : name,
+        'text'  : a.get_text() or '',
+        'href'  : href
+    }
+    acc[['external', 'internal'][int(is_internal_link(href))]].append(link_entry)
+    return acc
+def clean_soup(soup):
+    '''
+    Cleans the soup object by removing unwanted tags and attributes
+    '''
+    # Remove childless divs
+    for div in [x for x in soup.find_all('div') if len(div.get_children()) == 0]:
+        div.decompose()
+    # Remove unwanted tags
+    for tag in soup.find_all(UNWANTED_TAGS):
+        tag.decompose()
+    for tag in soup.find_all(UNWANTED_ENCLOSING_TAGS):
+        tag.unwrap()
+
+# endregion helper functions
+
 
 # Function to scrape a single webpage
 def scrape_page(url : str) -> object:
@@ -24,24 +57,6 @@ def scrape_page(url : str) -> object:
         Formatted JSON object containing scraped data
     TODO: explain JSON object format here
     '''
-    # region helper functions
-    def is_internal_link(href, base_url):
-        parsed_href = urlparse(href)
-        parsed_base = urlparse(base_url)
-        return parsed_href.netloc == '' or parsed_href.netloc == parsed_base.netloc
-    def link_reduce(acc, a):
-        # Ignore links to page HTML elements and <a> tags with no href
-        if not (href := a.get('href')) or href.startswith("#"):
-            return acc
-        link_entry  ={
-            
-            'title' : a.get('title'),
-            'text'  : a.get_text(),
-            'url'   : urljoin(url, href)
-        }
-        acc[['internal', 'external'][int(is_internal_link(href, url))]].append(href)
-        return acc
-    # endregion helper functions
     try:
         # Fetch the webpage
         response = requests.get(url)
@@ -49,11 +64,14 @@ def scrape_page(url : str) -> object:
         soup = BeautifulSoup(response.content, 'html.parser')
     except Exception as e:
         return {"error": f"Failed to scrape {url}: {e}"}
-    # extract all the links on the page
+    a_tags = soup.find_all('a')
+    links = reduce(link_reduce, a_tags, {'internal': [], 'external': []})
+
+    # extract all the links on the page before further processing
     def serialize(element):
         if not (tag := element.name):
             return element
-        return {  
+        return {
             'tag'           : element.name,
             'attributes'    : element.attrs,
             'children'      : [el for el in element.children if el.name not in UNWANTED_ENCLOSING_TAGS],
